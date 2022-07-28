2c651470b165034286a9fa3dedaa6e13a69e4a35
Ben Colegate <ben@bencolegate.co.uk>
2022-07-28 14:29:42 +0100

    Caster now also has the required flag

--- tests/Castables/CasterTest.php
+++ tests/Castables/CasterTest.php
@@ -2,6 +2,7 @@
 
 use Fixtures\NamedPet;
 use Xttribute\Xttribute\Castables\Caster;
+use Xttribute\Xttribute\Exceptions\IdentifyValueException;
 
 test('it casts to an object', function () {
     $doc = loadXmlFixture('pet.xml');
@@ -12,4 +13,27 @@ test('it casts to an object', function () {
         ->toBeInstanceOf(NamedPet::class)
         ->and($pet->name)
         ->toEqual('Bear');
+});
+
+test('it throws exception when property is required', function () {
+    $doc = loadXmlFixture('empty.xml');
+    $caster = new Caster(
+        '/scenarios/does-not-exist',
+        castTo: NamedPet::class,
+        required: true
+    );
+
+    $nulled = $caster->value($doc);
+})->throws(IdentifyValueException::class);
+
+test('it handle nulls when property is not required', function () {
+    $doc = loadXmlFixture('empty.xml');
+    $caster = new Caster(
+        '/scenarios/does-not-exist',
+        castTo: NamedPet::class,
+        required: false
+    );
+
+    $nulled = $caster->value($doc);
+    expect($nulled)->toBeNull();
 });
