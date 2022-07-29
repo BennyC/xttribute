62b77a5ba6e3a62e6be27b9e8f265616457e6f4a
Ben Colegate <ben@bencolegate.co.uk>
2022-07-29 08:09:17 +0100

    Added main examples

---
+++ examples/02-nested-objects/nested-objects.php
@@ -0,0 +1,37 @@
+<?php
+
+use Xttribute\Xttribute\Castables\Caster;
+use Xttribute\Xttribute\Castables\PathValue;
+use Xttribute\Xttribute\DOMDocumentCaster;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+class Customer
+{
+    public function __construct(
+        #[PathValue('/customer/name')]
+        public readonly string $name,
+        #[Caster('/customer/address', Address::class)]
+        public readonly Address $address
+    ) {
+    }
+}
+
+class Address
+{
+    public function __construct(
+        #[PathValue('/address/lineOne')]
+        public readonly string $lineOne,
+        #[PathValue('/address/lineTwo')]
+        public readonly string $lineTwo,
+    ) {
+    }
+}
+
+$doc = new DOMDocument();
+$doc->loadXML(file_get_contents(__DIR__ . '/nested-objects.xml'));
+
+$caster = new DOMDocumentCaster();
+$pet = $caster->cast($doc, Customer::class);
+
+echo json_encode($pet, JSON_PRETTY_PRINT);
