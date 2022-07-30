ffafd42809264a97a6366d61c1d43933c3e4017a
Ben Colegate <ben@bencolegate.co.uk>
2022-07-30 17:30:15 +0100

    Created benchmark tests

--- tests/Benchmarks/DOMDocumentCasterBench.php
+++ tests/Benchmarks/DOMDocumentCasterBench.php
@@ -2,10 +2,32 @@
 
 namespace Benchmarks;
 
+use DOMDocument;
+use DOMXPath;
+use Fixtures\NameAndAgePet;
+use Xttribute\Xttribute\DOMDocumentCaster;
+
 class DOMDocumentCasterBench
 {
-    public function benchConsume()
+    public function benchDOMDocumentCaster()
     {
-        sleep(1);
+        $doc = new DOMDocument();
+        $caster = new DOMDocumentCaster();
+        $doc->load(__DIR__ . '/../Fixtures/pet.xml');
+
+        $pet = $caster->cast($doc, NameAndAgePet::class);
+    }
+
+    public function benchDOMDocumentManualCastingWithXPath()
+    {
+        $doc = new DOMDocument();
+        $doc->load(__DIR__ . '/../Fixtures/pet.xml');
+        $name = (new DOMXPath($doc))->query('/pet/name');
+        $age = (new DOMXPath($doc))->query('/pet/stats/@age');
+
+        $pet = new NameAndAgePet(
+            $name->item(0)->nodeValue,
+            $age->item(0)->nodeValue,
+        );
     }
 }
