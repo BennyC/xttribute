98959de9949555e3b1d06a78db4386810333952a
code_ <jacy@local.com>
2023-01-16 00:24:23 +0330

    chore(examples): Enum example addition to array-of-objects.php

--- examples/04-array-of-objects/array-of-objects.php
+++ examples/04-array-of-objects/array-of-objects.php
@@ -2,6 +2,7 @@
 
 use Xttribute\Xttribute\Castables\ArrayOf;
 use Xttribute\Xttribute\Castables\Str;
+use Xttribute\Xttribute\Castables\Enum;
 use Xttribute\Xttribute\DOMDocumentCaster;
 
 require __DIR__ . '/../../vendor/autoload.php';
@@ -24,8 +25,16 @@ class OrderItem
         public readonly string $id,
         #[Str('/item/name')]
         public readonly string $name,
+        #[Enum('/item/type', ItemType::class)]
+        public readonly ItemType $type,
     ) {
     }
+}
+
+enum ItemType: string
+{
+    case Candy = 'Candy';
+    case Gum = 'Gum';
 }
 
 $doc = new DOMDocument();
